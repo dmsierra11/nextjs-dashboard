a32f696b0ded8b98bab9262951810355ae9f1783
Daniel Sierra <danielsierra@Setup-User.local>
2024-03-22 17:42:47 +0100

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+// Hand writing font
+import { Handlee } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const handlee = Handlee({ weight: '400', subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
